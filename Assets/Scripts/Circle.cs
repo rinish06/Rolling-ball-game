0a9e50554bd4d46a5da7a7c5ffdf325726ecb48d
Samir Kashyap <samir.kashyap17@gmail.com>
2020-01-16 21:35:47 -0600

    Added new pickup, changed 2 textures.

---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    public float speed = (2 * Mathf.PI)/10;
+    private float Theta = 0f; //starting theta
+    private int radius = 9; //radius
+
+    void Update()
+    {
+        Theta += speed * Time.deltaTime;
+        float x = radius * Mathf.Sin(Theta);
+        float z = radius * Mathf.Cos(Theta);
+        transform.position = new Vector3(x, 0.75f, z);
+    }
+}
