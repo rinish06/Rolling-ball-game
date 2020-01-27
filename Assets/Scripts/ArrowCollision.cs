aa1b6488b60fa53be66c8b18cc5e2fc7fbb5ed81
rinish06 <54551163+rinish06@users.noreply.github.com>
2020-01-27 16:00:20 -0600

    Added collisiion script

---
+++ Assets/Scripts/ArrowCollision.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrowCollision : MonoBehaviour
+{
+    public Collider targetCollider;
+
+    private int count;
+    public Text countText;
+    public Text winText;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.CompareTag("pickup"))
+        {
+            collision.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count >= 14)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
+
+
